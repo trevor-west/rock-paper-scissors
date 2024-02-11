213da673ad46b209cae4a08b258a221cb320b945
Trevor West <trevorwest@duck.com>
2024-02-11 10:36:15 -0600

    Add 3 step process to break problem down
    Specifically, I have added the steps to UNDERSTAND and PLAN for the first
    part of this problem. I will next work on the PSUEDOCODE for this part.

---
+++ script.js
@@ -0,0 +1,16 @@
+/* 
+1. UNDERSTAND:
+
+First I will understand the instructions in the first main step from the project page (step 3 in https://www.theodinproject.com/lessons/foundations-rock-paper-scissors):
+"Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
+We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!""
+
+So I need to make a function called getComputerChoice that returns randomly 'Rock', 'Paper', or 'Scissors
+
+2. PLAN
+- No graphical interface is needed -- this will all be done in the console
+- I could use Math.Random to associate 33% interval with each of the 3 values ('Rock', 'Paper', 'Scissors') and then use conditionals and boolean checks to return accordingly (randomly).
+
+3. PSEUDOCODE
+
+*/
